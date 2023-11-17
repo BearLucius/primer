0650112d172f6b81fe21b2269f64a72b967a6323
Lucius <enotik455@mail.ru>
2023-11-18 02:05:00 +0300

    Second message!!!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
 System.out.println("Допустим кря!");
-        System.out.println("Допустим кря! НО ВТОРОЕ");
+        System.out.println("Допустим кря! НО ВТОРОЕ!!!");
 
     }
 
